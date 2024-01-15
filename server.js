edb6b5ef073eb3276b52eb065be14223947d2b50
nghia <naovu@protonmail.com>
2024-01-15 20:58:59 +0700

    DONE: Section #2 for using basic lib and file structure

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./src/app.js");
 const PORT = 3055;
 
 const server = app.listen(PORT, () => {
-  console.log(PORT)
+  console.log(`Running PORT: ${PORT}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
 const app = express();
+
+app.use(morgan("combine"))
+app.use(helmet())
+app.use(compression())
+
+
+app.get("/", (req,res,next) =>{
+  let data = "lorem"
+  return res.status(200).json({msg: "Hi", metadata: data.repeat(100000)})
+})
 
 module.exports = app;
