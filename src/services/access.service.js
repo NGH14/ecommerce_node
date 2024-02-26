013968f3ed64a3f196b8faa0c69c2613f347e83d
nghia <naovu@protonmail.com>
2024-02-26 23:38:01 +0700

    IMPLEMENT: RSA in access service (not done yet)

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -1,6 +1,14 @@
 const bcrypt = require("bcrypt")
+const crypto = require("crypto")
 
 const shopModel = require('../models/shop.models');
+
+const Role = {
+	SHOP: "0001",
+	WRITER: "008",
+	EDITOR: "007",
+	ADMIN: "000"
+}
 
 
 class AccessService {
@@ -12,7 +20,16 @@ class AccessService {
 					code: 'yyyy',
 					message: 'Shop already registered ',
 				};
-        
+        const passwordHash = await bcrypt.hash(password, 10);
+
+				const newShop = await shopModel.create({
+					name, password: passwordHash, roles: [Role.SHOP]
+				})
+
+				if (newShop) {
+					const { publicKey, privateKey } = crypto.generateKeyPair('rsa', {modulusLength: 4096})
+					console.log({ publicKey, privateKey })
+				}
 
 
 		} catch (error) {
