149aac687fa4997dade759e1356f9c08bc6bbd44
nghia <naovu@protonmail.com>
2024-06-14 16:26:43 +0700

    Remove: test file

--- test.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-  modulusLength: 2048,
-});
-
-console.log({ public: publicKey.toString(), private: privateKey.toString() });
